65bc052d05eef0899d997f10ea7b813ebeb0c33f
João Vidal <dev.joaovidal@gmail.com>
2022-06-17 21:22:53 -0300

    adding click event listener

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ let currentNumber = 0;
 document.addEventListener('keypress', function (e) {
     if (e.key === '+') {
         const btnplus = document.querySelector('#plusButton')
-
+        
         btnplus.click();
     } 
     else if (e.key === '-') {
         const btnminus =  document.querySelector('#minusButton')
-
+        
         btnminus.click();
     }
     
 })
+
+
+    const btnplus = document.querySelector('#plusButton')
+    const btnminus =  document.querySelector('#minusButton')
+
+    btnplus.addEventListener('click', increment)
+    btnminus.addEventListener('click', decrement)
+
 
 function increment() {
     currentNumber = currentNumber + 1;
